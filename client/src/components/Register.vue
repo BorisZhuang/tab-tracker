fafb773f447e358165aceccfda41d5537158a7fa
Boris Zhuang <bzhuangliang@gmail.com>
2019-04-24 23:47:50 -0700

    created a Register vuejs component.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input v-model="email" type="email" name="email" placeholder="email" />
+    <br>
+    <input v-model="password" type="password" name="password" placeholder="password" />
+    <br>
+    <button v-on:click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenService from '@/services/AuthenticationService'
+
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
