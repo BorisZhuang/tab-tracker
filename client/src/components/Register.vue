89d0baec6a6abe6c03fbc9f384762123d01dd400
Boris Zhuang <bzhuangliang@gmail.com>
2019-04-29 00:29:52 -0700

    start using vuetify to apply material design on the UI.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input v-model="email" type="email" name="email" placeholder="email" />
-    <br>
-    <input v-model="password" type="password" name="password" placeholder="password" />
-    <br>
-    <div class="error" v-html="error" />
-    <br>
-    <button v-on:click="register">Register</button>
-  </div>
+  <v-layout row wrap>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <input v-model="email" type="email" name="email" placeholder="email" />
+            <br>
+            <input v-model="password" type="password" name="password" placeholder="password" />
+            <br>
+            <div class="error" v-html="error" />
+            <br>
+            <v-btn v-on:click="register" class="cyan">Register</v-btn>
+          </div>
+        </div>
+      </v-flex>
+  </v-layout>
 </template>
 
 <script>
