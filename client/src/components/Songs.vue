5d3b699dd41dc574dfcc1db7b813c3dcfae64edc
Boris Zhuang <bzhuangliang@gmail.com>
2019-05-06 23:45:23 -0700

    Updates the songs list.

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -14,8 +14,30 @@
             >
             <v-icon>add</v-icon>
           </v-btn>
-          <div v-for="song in songs" :key="song.id">
-            {{song.title}} - {{song.artist}} - {{song.album}}
+          <div class="song" v-for="song in songs" :key="song.id">
+            <v-layout>
+              <v-flex xs6>
+                <div class="song-title">
+                  {{song.title}}
+                </div>
+                <div class="song-artist">
+                  {{song.artist}}
+                </div>
+                <div class="song-genre">
+                  {{song.genre}}
+                </div>
+                <v-btn
+                  class="cyan ma-4"
+                  dark
+                  @click="navigateTo({name: 'song', params: {songId: song.id}})">
+                  View
+                </v-btn>
+              </v-flex>
+
+              <v-flex xs6>
+                <img class="album-image" :src="song.albumImage"/>
+              </v-flex>
+            </v-layout>
           </div>
         </panel>
       </v-flex>
@@ -48,4 +70,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px;
+}
+.song-artist {
+  font-size: 24px;
+}
+.song-title {
+  font-size: 18px;
+}
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
