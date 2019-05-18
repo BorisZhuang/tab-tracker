a31058aa38267a6ef87c644ba960495363db8789
Boris Zhuang <bzhuangliang@gmail.com>
2019-05-18 13:19:46 -0700

    Created bookmarks seed to populate db with predefined bookmark data.

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,10 +1,11 @@
-const { sequelize, Song, User } = require('../src/models')
+const { sequelize, Song, User, Bookmark } = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs')
 const users = require('./users')
+const bookmarks = require('./bookmarks')
 
-sequelize.sync({force: true})
+sequelize.sync({ force: true })
   .then(async function () {
     await Promise.all(
       users.map(user => {
@@ -14,6 +15,11 @@ sequelize.sync({force: true})
     await Promise.all(
       songs.map(song => {
         Song.create(song)
+      })
+    )
+    await Promise.all(
+      bookmarks.map(bookmark => {
+        Bookmark.create(bookmark)
       })
     )
   })
