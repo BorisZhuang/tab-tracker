9636e5b75ab211e69ddac06c39733a980856d180
Boris Zhuang <bzhuangliang@gmail.com>
2019-05-27 17:26:28 -0700

    Added a show/hide pwd append icon and validation rules.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -4,14 +4,20 @@
         <panel title="Register">
           <form name="tab-tracker-form" autocomplete="off">
             <v-text-field
+              ref="email"
               label="Email"
               v-model="email"
+              :rules="[rules.required, rules.email]"
             ></v-text-field>
             <br>
             <v-text-field
+              ref="password"
               label="Password"
-              type="password"
               v-model="password"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+              :rules="[rules.required, rules.lengthLimits]"
+              @click:append="showPassword = !showPassword"
             ></v-text-field>
             <br>
             <div class="error" v-html="error" />
@@ -32,12 +38,39 @@ export default {
     return {
       email: '',
       password: '',
-      error: null
+      error: null,
+      showPassword: false,
+      formHasErrors: false,
+      rules: {
+        required: value => !!value || 'Required.',
+        lengthLimits: v => (v.length >= 8 && v.length <= 32) || 'Min 8 charactersIt must be at least 8 characters in length and not greater than 32 characters in length.',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        }
+      }
+    }
+  },
+  computed: {
+    form () {
+      return {
+        email: this.email,
+        password: this.password
+      }
     }
   },
   methods: {
     async register () {
       try {
+        this.formHasErrors = false
+
+        Object.keys(this.form).forEach(f => {
+          if (!this.form[f]) this.formHasErrors = true
+          this.$refs[f].validate(true)
+        })
+
+        if (this.formHasErrors) return
+
         const response = await AuthenService.register({
           email: this.email,
           password: this.password
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -2,10 +2,11 @@ const Joi = require('joi')
 
 module.exports = {
   register (req, res, next) {
+    const pattern = '^[a-zA-Z0-9]{8,32}$'
     const schema = {
       email: Joi.string().email(),
       password: Joi.string().regex(
-        new RegExp('^[a-zA-Z0-9]{8,32}$')
+        new RegExp(pattern)
       )
     }
 
